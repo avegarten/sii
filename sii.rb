b9905ba478e29b670f469464e8a5dec3cfd0b6d9
avery <aoi.jeo@gmail.com>
2025-12-17 20:15:50 -0600

    working put endpoint !! celebrate

--- sii.rb
+++ sii.rb
@@ -14,19 +14,26 @@ init_files_dir = FileUtils.mkdir_p('./files').first
 # test dir for now
 test_dir = '/home/avery/sii/files'
 
-#upload <local_src> <sii_dest>
-#usage curl -X PUT -T /home/avery/Downloads/cat.jpeg http://0.0.0.0:1100/files/images/cat.jpeg
- 
+
 put '/files/*' do
+
   request.body.rewind
 
   dir = params['splat'].first
-
   file = request.body
 
-  IO::copy_stream(file, "#{test_dir}/#{dir}")
-  # stream files instead File.write
+  final_dir = "#{test_dir}/#{dir}"
+  init_path = File.dirname(final_dir)
 
+  puts final_dir
+  puts init_path
+  # for now, testing paths to see whats its giving
+
+
+  FileUtils.mkdir_p(init_path)
+  IO::copy_stream(file, final_dir)
+  
   halt 404 if file.size == 0
+
   # basic 404 for now
 end
